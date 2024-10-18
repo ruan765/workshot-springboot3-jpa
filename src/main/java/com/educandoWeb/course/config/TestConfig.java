a08d7eb1ecc04d421745e11a2de10b2218782c44
Ruan pablo <ruansaraiva555@gmail.com>
2024-10-17 22:00:30 -0300

    Payment, One-to-One Association

--- src/main/java/com/educandoWeb/course/config/TestConfig.java
+++ src/main/java/com/educandoWeb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoWeb.course.entities.Category;
 import com.educandoWeb.course.entities.Order;
 import com.educandoWeb.course.entities.OrderItem;
+import com.educandoWeb.course.entities.Payment;
 import com.educandoWeb.course.entities.Product;
 import com.educandoWeb.course.entities.User;
 import com.educandoWeb.course.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(c2);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
