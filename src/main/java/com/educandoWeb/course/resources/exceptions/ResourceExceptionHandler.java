3cba0011d4f680061b718dab28cc1ffa61f78faa
Ruan pablo <ruansaraiva555@gmail.com>
2024-12-17 19:39:26 -0300

    Exception Handling Update

--- src/main/java/com/educandoWeb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoWeb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -22,12 +22,15 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
 	@ExceptionHandler(DataBaseException.class)
-	public ResponseEntity<StandardError> DataBase(DataBaseException e, HttpServletRequest request) {
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request) {
 		String error = "Data base error";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	
 
 }
--- src/main/java/com/educandoWeb/course/services/UserService.java
+++ src/main/java/com/educandoWeb/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.educandoWeb.course.repositories.UserRepository;
 import com.educandoWeb.course.services.exceptions.DataBaseException;
 import com.educandoWeb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,14 @@ public class UserService {
 	}}
 	
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+			
+		}
 	}
 
 	private void updateData(User entity, User obj) {
